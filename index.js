fc21e414517c8a70633b9e8f45f7f257efc362d7
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-03 21:32:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+   var output = "";
+    for(var i = 0; i < dwarves.length; i++) {
+      output += (i +1) + ". " + dwarves[i] + " ";  
+    }
+    return output;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  
+  let uppercasePlaneteerCalls = [];
+    for(var i = 0; i < planeteerCalls.length; i++) {
+       uppercasePlaneteerCalls.push(`${planeteerCalls[i]}!`.toUpperCase());
+    }
+      return uppercasePlaneteerCalls;    
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+  
+    if(words[i].length>4){
+      return true;
+    }
+  }
+  return false;
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert') {
+      return foods[i];
+    }
+  }
+  return 'no cheese!';
 }
+
