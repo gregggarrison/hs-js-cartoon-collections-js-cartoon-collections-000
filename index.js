bb5b3e747de49cdc18316ddb9f5302ee9e7cfc78
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-06 21:48:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 function dwarfRollCall(dwarves) {
-   var output = "";
-    for(var i = 0; i < dwarves.length; i++) {
-      output += (i +1) + ". " + dwarves[i] + " ";  
-    }
-    return output;
+  var newStr = "";
+  for(var i = dwarves.length/2; i < dwarves.length; i++) {
+    newStr += (i + 1) + ". " + dwarves[i] + " ";
+  }
+  return newStr;
+}
+console.log(dwarfRollCall(dwarves)); 
+
+  var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+
+function summonCaptainPlanet(planeteerCalls) { 
+  var capsPCalls = [];
+  for(var i = 0; i < planeteerCalls.length; i++) {
+    var uppPlaneteerCalls = planeteerCalls[i].toUpperCase();
+    var upperPunct = uppPlaneteerCalls + "!";
+    capsPCalls.push(upperPunct);
+  }
+  return capsPCalls;
 }
 
-function summonCaptainPlanet(planeteerCalls){
-  
-  let uppercasePlaneteerCalls = [];
-    for(var i = 0; i < planeteerCalls.length; i++) {
-       uppercasePlaneteerCalls.push(`${planeteerCalls[i]}!`.toUpperCase());
-    }
-      return uppercasePlaneteerCalls;    
-}
+var shortWords = ["wind", "fire"];
+var assortedWords = ["earth", "wind", "heart", "fire"];
 
 function longPlaneteerCalls(words) {
-  for(let i = 0; i < words.length; i++){
-  
-    if(words[i].length>4){
+  for(var i = 0; i < words.length; i++) {
+    if(words[i].length > 4) {
       return true;
     }
   }
   return false;
 }
 
-function findTheCheese(foods) {
-  for (var i = 0; i < foods.length; i++) {
-    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert') {
-      return foods[i];
+var snacks = ["crackers", "gouda", "thyme"];
+var soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"];
+var ingredients = ["garlic", "rosemary", "bread"];
+
+function findTheCheese(food) {
+  for(var i = 0; i < food.length; i++) {
+    if(food[i] === 'cheddar' || food[i] === 'gouda' || food[i] === 'camembert' || food[i] === 'swiss') {
+      return food[i];
     }
   }
   return 'no cheese!';
+}
+
+function wordsWB (words) {
+  let arrB = [];
+  for(var i = 0; i < words.length; i++) {
+    if(words[i].startsWith('b')) {
+        arrB.push[word[i]]
+
+  }
+  }
+  
+  return arrB;
 }
 
